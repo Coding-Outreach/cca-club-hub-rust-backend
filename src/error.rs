a3c8be023ea657e6f3ee47cb16d4938de293d161
Ryan Bai <ryanbai1412@gmail.com>
2022-10-15 21:11:30 -0700

    add authorization checker for requests

--- src/error.rs
+++ src/error.rs
@@ -6,14 +6,16 @@ use axum::{
 use serde::Serialize;
 use std::borrow::Cow;
 
-pub enum AppError {
-    InternalServerError(anyhow::Error),
-    ResponseStatusError(StatusCode, Cow<'static, str>),
+#[derive(Debug, Clone)]
+pub struct ResponseStatusError(StatusCode, Cow<'static, str>);
+
+impl ResponseStatusError {
+    pub fn from(code: StatusCode, s: impl Into<Cow<'static, str>>) -> Self {
+        Self(code, s.into())
+    }
 }
 
-pub type AppResult<T> = Result<T, AppError>;
-
-impl IntoResponse for AppError {
+impl IntoResponse for ResponseStatusError {
     fn into_response(self) -> Response {
         #[derive(Serialize)]
         struct AppErrorResponse {
@@ -21,21 +23,46 @@ impl IntoResponse for AppError {
             message: Cow<'static, str>,
         }
 
+        (
+            self.0,
+            Json(AppErrorResponse {
+                status: self.0.as_u16(),
+                message: self.1,
+            }),
+        )
+            .into_response()
+    }
+}
+
+impl<S: Into<Cow<'static, str>>> From<(StatusCode, S)> for ResponseStatusError {
+    fn from((code, s): (StatusCode, S)) -> Self {
+        Self::from(code, s)
+    }
+}
+
+pub enum AppError {
+    InternalServerError(anyhow::Error),
+    ResponseStatusError(ResponseStatusError),
+}
+
+pub type AppResult<T> = Result<T, AppError>;
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
         match self {
             AppError::InternalServerError(_err) => {
                 // TODO: log error
                 AppError::from(StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error")
                     .into_response()
             }
-            AppError::ResponseStatusError(code, s) => (
-                code,
-                Json(AppErrorResponse {
-                    status: code.as_u16(),
-                    message: s,
-                }),
-            )
-                .into_response(),
+            AppError::ResponseStatusError(rse) => rse.into_response(),
         }
+    }
+}
+
+impl AppError {
+    pub fn from(code: StatusCode, s: impl Into<Cow<'static, str>>) -> AppError {
+        AppError::ResponseStatusError(ResponseStatusError::from(code, s))
     }
 }
 
@@ -45,8 +72,8 @@ impl<E: Into<anyhow::Error>> From<E> for AppError {
     }
 }
 
-impl AppError {
-    pub fn from(code: StatusCode, s: impl Into<Cow<'static, str>>) -> AppError {
-        AppError::ResponseStatusError(code, s.into())
+impl From<ResponseStatusError> for AppError {
+    fn from(e: ResponseStatusError) -> AppError {
+        AppError::ResponseStatusError(e)
     }
 }
