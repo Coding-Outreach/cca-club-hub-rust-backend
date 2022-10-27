f2813a7b78320275352c96877230c80a071a491c
voxal <aidenhohoho@gmail.com>
2022-10-26 22:29:03 -0700

    cors and about field

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ async fn main() {
     let pool = connect_to_db(&config.db_url);
     let cors = CorsLayer::new()
         .allow_methods([Method::GET, Method::POST])
+        .allow_headers(Any)
         .allow_origin(Any);
     let app = cca_club_hub::app().layer(Extension(pool)).layer(cors);
 
